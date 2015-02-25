5b275bfab0c5518dac6d76cd5aa6a1cc8b9dd02f
Christian Kniep <christian@qnib.org>
2015-02-25 21:16:51 +0100

    opensm as default SIM_HOST is kinda stupid :)

--- opt/qnib/bin/start_opensm.sh
+++ opt/qnib/bin/start_opensm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LD_PRELOAD=/usr/lib64/umad2sim/libumad2sim.so
-export SIM_HOST=${SIM_HOST-opensm}
+#export SIM_HOST=${SIM_HOST-opensm}
 
 function wait_ibsim() {
     if [ $(ps -ef|grep -v grep|grep -c ibsim) -eq 0 ];then
@@ -11,6 +11,6 @@ function wait_ibsim() {
 }
 
 wait_ibsim
-sleep 5
+sleep 15
 
 /usr/local/sbin/opensm -F /etc/opensm/opensm.conf
